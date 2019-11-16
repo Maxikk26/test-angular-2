91d893cf7957e38fbf5ce6373568d0ca566f6cf5
Maxikk26 <maximiliano.bogo@gmail.com>
2019-11-16 14:53:34 -0400

    sidebar colapsable con animacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import *  as JQuery from '../templates/js/jquery-3.1.1.min.js';
+
+function SmoothlyMenu() {
+  if (!JQuery('body').hasClass('mini-navbar') || JQuery('body').hasClass('body-small')) {
+      // Hide menu in order to smoothly turn on when maximize menu
+      JQuery('#side-menu').hide();
+      // For smoothly turn on menu
+      setTimeout(
+          function() {
+            JQuery('#side-menu').fadeIn(400);
+          }, 200);
+  } else if (JQuery('body').hasClass('fixed-sidebar')) {
+    JQuery('#side-menu').hide();
+      setTimeout(
+          function() {
+            JQuery('#side-menu').fadeIn(400);
+          }, 100);
+  } else {
+      // Remove all inline style from JQuery fadeIn function to reset menu state
+      JQuery('#side-menu').removeAttr('style');
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-angular-v2';
+
+  ngOnInit(){
+    JQuery('.navbar-minimalize').on('click', function(event) {
+      event.preventDefault();
+      JQuery("body").toggleClass("mini-navbar");
+      SmoothlyMenu();
+
+    });
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var JQuery;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
