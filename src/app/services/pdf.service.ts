4b825badf71ffb7f44689d5f83002d173f2b5915
Maxikk26 <maximiliano.bogo@gmail.com>
2019-11-27 10:15:00 -0400

    Añadido descarga como pdf

--- src/app/services/pdf.service.ts
+++ src/app/services/pdf.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Content } from './content.service';
+import 'jspdf-autotable';
+import * as jsPDF from 'jspdf';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,16 @@ import { Injectable } from '@angular/core';
 export class PdfService {
 
   constructor() { }
+
+  downloadPDF(content:Content[]){
+    let doc = new jsPDF();
+    let col = ["Rendering Engine","Browser","Platform(s)","Engine Version","CSS Grade"];
+    let rows = [];
+    for(let i in content){
+      let temp = [content[i].rendering,content[i].browser,content[i].platform,content[i].engine,content[i].css];
+      rows.push(temp);
+    }
+    doc.autoTable(col,rows);
+    doc.save('ExampleFile.pdf');
+  }
 }
